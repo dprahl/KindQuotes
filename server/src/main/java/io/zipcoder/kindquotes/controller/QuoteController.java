1482569a29557983409adb8d2855e93605563092
Daniel Prahl <daniel.prahl@gmail.com>
2017-06-24 22:08:54 -0400

    "adding quoteRepository and adding CRUD method mapping to quoteController"

--- server/src/main/java/io/zipcoder/kindquotes/controller/QuoteController.java
+++ server/src/main/java/io/zipcoder/kindquotes/controller/QuoteController.java
@@ -1,12 +1,14 @@
 package io.zipcoder.kindquotes.controller;
 
 import io.zipcoder.kindquotes.model.Quote;
+import io.zipcoder.kindquotes.repository.QuoteRepository;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 
 @RequestMapping("/quotes")
@@ -14,12 +16,72 @@ import java.util.ArrayList;
 @CrossOrigin("http://localhost:8100")
 public class QuoteController {
 
+    @Inject
+    private QuoteRepository quoteRepository;
+
+    /*
     @RequestMapping("/")
     public ResponseEntity<ArrayList<Quote>> getAllQuotes(){
         Quote quote = new Quote("Wu Tang Clan!!!");
         ArrayList<Quote> quotes = new ArrayList<>();
         quotes.add(quote);
         return new ResponseEntity<>(quotes, HttpStatus.OK);
+    }
+    */
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public ResponseEntity<ArrayList<Quote>> getAllQuotes() {
+        if(quoteRepository.count() > 0) {
+            ArrayList<Quote> allQuotes = (ArrayList<Quote>) quoteRepository.findAll();
+            return new ResponseEntity<>(allQuotes, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @RequestMapping(value="/{quoteId}", method= RequestMethod.GET)
+    public ResponseEntity<Quote> getQuote(@PathVariable Long quoteId) {
+        if(quoteRepository.exists(quoteId)) {
+            Quote quote = quoteRepository.findOne(quoteId);
+            return new ResponseEntity<>(quote, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value="/", method = RequestMethod.POST)
+    public ResponseEntity<Quote> createQuote(@RequestBody Quote quote, UriComponentsBuilder ucBuilder){
+        quote = quoteRepository.save(quote);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("quote/{id}")
+                .buildAndExpand(quote.getId())
+                .toUri());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{quoteId}", method = RequestMethod.PUT)
+    public ResponseEntity<Quote> updateQuote(@RequestBody Quote quote, @PathVariable Long quoteId){
+        if(quoteRepository.exists(quoteId)) {
+            quote = quoteRepository.save(quote);
+            return new ResponseEntity<>(quote, HttpStatus.ACCEPTED);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value="/{quoteId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Quote> deleteQuote(@PathVariable Long quoteId) {
+        quoteRepository.delete(quoteId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/", method = RequestMethod.DELETE)
+    public ResponseEntity<Quote> deleteAllQuotes() {
+        if(quoteRepository.count() > 0) {
+            quoteRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 
 }
--- server/src/main/java/io/zipcoder/kindquotes/model/Quote.java
+++ server/src/main/java/io/zipcoder/kindquotes/model/Quote.java
@@ -1,12 +1,32 @@
 package io.zipcoder.kindquotes.model;
 
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Quote {
 
+    @Id
+    @GeneratedValue
+    @Column(name="QUOTE_ID")
+    private Long id;
+
+    @Column(name="MESSAGE")
     private String message;
 
     public Quote(String msg){
         this.message = msg;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
     }
 
     public String getMessage() {
